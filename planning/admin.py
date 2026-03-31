35c58a02d1a4645ffee2f12525412e142dfaccbf
Bruno Luiz <bruno.luiz.3110@gmail.com>
2026-03-31 19:37:44 -0300

    Create Admin

--- planning/admin.py
+++ planning/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Atividade)
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ['numero_chamdo', 'descricao', 'disciplina', 'local', 'data_prog', 'hh_previstas', 'encarregado', 'data_criacao']
+    list_filter = ['disciplina', 'data_prog', 'encarregado']
+    search_fields = ['numero_chamdo', 'encarregado']
